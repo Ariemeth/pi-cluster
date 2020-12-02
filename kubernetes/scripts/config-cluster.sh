db0c03ed63e0eecb5c74f579d2b25b7ae57cdf9a
Edward Carmack <ariemethacc@gmail.com>
2020-12-02 01:35:28 -0500

    configured cert manager and setup tls routes

--- kubernetes/scripts/config-cluster.sh
+++ kubernetes/scripts/config-cluster.sh
@@ -14,11 +14,14 @@ helm repo add traefik https://helm.traefik.io/traefik
 helm repo add openebs https://openebs.github.io/charts
 helm repo add jetstack https://charts.jetstack.io
 
+helm repo update
+
 # Create the namespaces.
 kubectl apply -f $CONFIG_DIR/growingsewfast/namespace.yaml
 kubectl apply -f $CONFIG_DIR/metadiversions/namespace.yaml
 kubectl apply -f $CONFIG_DIR/ingress/namespace.yaml
 kubectl create namespace openebs
+kubectl create namespace cert-manager
 
 # Install OpenEBS
 helm upgrade --install \
@@ -35,16 +38,37 @@ helm upgrade --install \
   traefik/traefik \
   --version "9.11.0"
 
-# Install the simpleweb service.
+helm upgrade --install \
+  -n cert-manager \
+  cert-manager \
+  jetstack/cert-manager \
+  --set installCRDs=true \
+  --version "v1.1.0"
+
+# Install the simpleweb service in the metadiversions namespace.
 helm upgrade --install \
   -n metadiversions \
   sw1 \
   $CHART_DIR/simpleweb
 
-# Install the ingresses
+# Install the simpleweb service in the growingsewfast namespace.
+helm upgrade --install \
+  -n growingsewfast \
+  sw1 \
+  $CHART_DIR/simpleweb
+
+# Get certificates
+kubectl apply -f $CONFIG_DIR/le-production.yaml
+kubectl -n metadiversions apply -f $CONFIG_DIR/metadiversions/le-cert.yaml
+kubectl -n growingsewfast apply -f $CONFIG_DIR/growingsewfast/le-cert.yaml
+
+# Install the ingresses for metadiversions.com
 kubectl apply \
-  -f $CONFIG_DIR/metadiversions/ingress.yaml \
+  -f $CONFIG_DIR/metadiversions/ingressRoute.yaml \
   -n metadiversions
 
-
+# Install the ingresses for growingsewfast.com
+kubectl apply \
+  -f $CONFIG_DIR/growingsewfast/ingressRoute.yaml \
+  -n growingsewfast
 
